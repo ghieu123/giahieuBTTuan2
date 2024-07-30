5c90d2ff8f3abebf4afdb98de4cd155c4d95e57f
ghieu123 <giahieuc5@gmail.com>
2024-07-30 09:47:56 +0700

    Cau1,1

---
+++ Cau1,1.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Định nghĩa kích thước mảng là một hằng số
+#define SIZE 15
+
+// Ham tao mang mot chieu ngau nhien co so phan tu lon hon hoac bang 15
+void taoMangNgauNhien(int mang[], int n, int min, int max) {
+    for (int i = 0; i < n; i++) {
+        mang[i] = min + rand() % (max - min + 1);
+    }
+}
+
+// Ham in mang
+void inMang(int mang[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", mang[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    srand((unsigned int)time(0));
+
+    int mang[SIZE];
+
+    // 1. Tao mang mot chieu ngau nhien
+    taoMangNgauNhien(mang, SIZE, 0, 100);
+    printf("Mang ngau nhien: ");
+    inMang(mang, SIZE);
+
+    return 0;
+}
